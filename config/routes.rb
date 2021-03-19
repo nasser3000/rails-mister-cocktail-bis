9156bf0a783c44108d526500e9900d358c1f32e8
nasser madaran <nas.madaran@gmail.com>
2021-03-19 21:19:59 +0100

    second update

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :doses, only: [ :index, :new, :create, :destroy ]
-  resources :ingredients, only: [ :index, :new, :create, :update, :destroy ]
-  resources :cocktails, only: [ :index, :new, :create, :update, :destroy ]
+  resources :cocktails, only: [:index, :show, :new, :create] do
+    resources :doses, only: [:new, :create]
+  end
+  resources :doses, only: [:destroy]
 end
